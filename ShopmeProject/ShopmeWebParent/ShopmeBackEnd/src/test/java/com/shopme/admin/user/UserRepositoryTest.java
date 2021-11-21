0f012c90b19493f21305db6acf5cbdfb75fe1b7c
LorenYan98 <LorenYan98>
2021-11-20 17:14:54 -0800

    Update the backend user interface

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTest.java
@@ -0,0 +1,67 @@
+package com.shopme.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Role;
+import com.shopme.common.entity.User;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE) // not run the test case in the real database
+@Rollback(false)
+public class UserRepositoryTest {
+	@Autowired
+	private UserRepository repo;
+	
+	@Autowired
+	private TestEntityManager entityManager;
+	
+	@Test
+	public void testCreateUser() {
+		Role roleAdmin = entityManager.find(Role.class, 1);
+		User userNamHM = new User("12342@qq.com","ma2n202", "N1am", "Ha Mi3nh");
+		userNamHM.addRole(roleAdmin);
+		User savedUser = repo.save(userNamHM);
+		assertThat(savedUser.getId()).isGreaterThan(0);
+	}
+	
+	@Test
+	public void testCreateUserWithTwoRoles() {
+		Role roleEditor = new Role(3);
+		Role roleAssistant = new Role(5);
+		User userRavi = new User("rav11i@qq.com","23rave202", "1Ravi", "Km1ar");
+		userRavi.addRole(roleEditor);
+		userRavi.addRole(roleAssistant);
+		User savedUser = repo.save(userRavi);
+
+		assertThat(savedUser.getId()).isGreaterThan(0);
+	}
+	
+	@Test
+	public void testListAllUsers() {
+		Iterable<User> listUsers = repo.findAll();
+		listUsers.forEach(user -> System.out.print(user));
+	}
+	
+	@Test
+	public void testGertUserById() {
+		User userNam = repo.findById(3).get();
+		System.out.println(userNam);
+		assertThat(userNam).isNotNull();
+	}
+	
+	@Test
+	public void TestDeleteUser() {
+		Integer userId = 14;
+		repo.deleteById(userId);
+		
+		repo.findById(userId);
+	}
+}
