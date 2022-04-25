14fb09aad4123248c29c0b12a080a8d029480100
Loren <80423481+LorenYan98@users.noreply.github.com>
2022-04-25 00:00:15 -0700

    Updated product description

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
@@ -6,18 +6,49 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.shopme.admin.brand.BrandService;
+import com.shopme.admin.category.CategoryService;
+import com.shopme.common.entity.Brand;
+import com.shopme.common.entity.Category;
 import com.shopme.common.entity.Product;
 
 @Controller
 public class ProductController {
 	@Autowired
 	private ProductService productService;
+	@Autowired
+	private BrandService brandService;
+	@Autowired CategoryService categoryService;
 	
 	@GetMapping("/products")
 	public String listPage(Model model) {
 		List<Product> listProduct = productService.listAll();
 		model.addAttribute("listProducts",listProduct);
 		return "products/products"; 
+	}
+	
+	@GetMapping("/products/new")
+	public String newProduct(Model model) {
+		List<Brand> listBrands = brandService.listAll();
+		List<Category> listCategories = categoryService.listCategoriesUsedInForm();
+		
+		Product product = new Product();
+		product.setEnabled(true);
+		product.setInStock(true);
+		
+		model.addAttribute("product", product);
+		model.addAttribute("listBrands", listBrands);
+		model.addAttribute("listCategories", listCategories);
+		model.addAttribute("pageTitle", "Create New Product");
+		
+		return "products/product_form"; 
+		
+	}
+	
+	@PostMapping("/products/save")
+	public String saveProducts(Product product) {
+		return "redirect:/products";
 	}
 }
